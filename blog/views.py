ce9237e2b69ffe239f4bc9c0632dfddc6c24df0e
SOMA NARESH <somanareshsrec.459@gmail.com>
2016-04-27 15:09:13 +0000

    Changes

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(id > 0 )
+    return render(request, 'blog/post_list.html', {'posts' : posts})
 def personal_details(request):
     return render(request, 'blog/personal_details.html', {})
